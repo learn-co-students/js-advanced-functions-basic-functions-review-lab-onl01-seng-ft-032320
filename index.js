c442ec1be55f170b95e908436bdac66d92a90614
Swishyfishie2 <pclandreiuk@gmail.com>
2020-07-03 04:31:01 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(arg = "roller-skate") {
+    return `This Saturday, I want to ${arg}!`
+}
+
+let mondayWork = function(arg = 'go to the office') {
+    return `This Monday, I will ${arg}.`
+}
