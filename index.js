9f5d7be6945012a60a777892137543c6b240be20
Swishyfishie2 <pclandreiuk@gmail.com>
2020-07-04 00:12:35 +0100

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ function saturdayFun(arg = "roller-skate") {
 let mondayWork = function(arg = 'go to the office') {
     return `This Monday, I will ${arg}.`
 }
+
+let wrapAdjective = function(str = "*") {
+    return function(par = "special") {
+        return `You are ${str}${par}${str}!`
+    }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
+
+let Calculator = {
+    add: () => { return 1 + 3 },
+    subtract: () => { return 1 - 3 },
+    multiply: () => { return 1 * 3 },
+    divide: () => { return 10 / 5 }
+
+}
+
+function actionApplyer(int, arr) {
+    let num = int;
+    if (arr.length === 0) {
+        return num
+    } else {
+        arr.forEach(el => {
+            num = el(num)
+
+        })
+    }
+    return num
+}
