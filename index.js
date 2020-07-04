286d002ec48d1e4f37dddf2a64d80d8fc7923375
antdp425 <antdp425@aol.com>
2020-07-04 09:06:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+let Calculator = {
+   add: (a,b) => {return a+b},
+   subtract: (a,b) => {return a-b},
+   multiply: (a,b)=> {return a*b},
+   divide: (a,b) => {return a/b}
+}
+
+function saturdayFun(activity = "roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity = "go to the office"){
+   return `This Monday, I will ${activity}.`  
+}
+
+let wrapAdjective = function(flair = "*") {
+   return function(value = "special"){
+      return `You are ${flair}${value}${flair}!`
+   }
+}
+
+
+function actionApplyer(int, array) {
+   if (array.length === 0){
+      return int
+   } else {
+      array.forEach(arr => {
+         int = arr(int)
+      });
+      return int
+   }
+}
